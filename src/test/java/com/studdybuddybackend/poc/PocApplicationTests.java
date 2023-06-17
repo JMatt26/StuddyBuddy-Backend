b242f82f4d035a6dcb8315d7b3407d152cbeb34f
Jared Matthews <jared.matthews@mail.mcgill.ca>
2023-06-17 15:45:23 -0400

    Set Up

---
+++ src/test/java/com/studdybuddybackend/poc/PocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studdybuddybackend.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
